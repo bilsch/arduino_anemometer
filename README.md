a591c994982d38654b745eb2ce29c33767d755c9
Bill Schwanitz <bilsch@bilsch.org>
2013-06-29 20:21:54 -0400

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 arduino_anemometer
 ==================
+
+This is a little anemometer project I've started.
+
+Sensor is from http://shop.moderndevice.com/products/wind-sensor
+
+My main idea behind this is to allow me to measure the air flow coming out of the vents in my house but it could be used for all kinds of neat stuff.
+
+This is still very early in its life cycle. I'll probably end up pulling the data from 2-3 of these so I can get approx the same air flow in the rooms upstairs since thats the problem area
---
+++ wind_sensor/wind_sensor.ino
@@ -0,0 +1,57 @@
+/* 
+ * Sensor to detect the wind speed
+ * Note, my main interest here is to detect the air flow from the vents in my house hvac system so I can tune the dampers
+ * I'm also not converting these numbers to anything overly meainingful - just reading the raw values
+ *
+ * Sensor calibration logic comes from this very handy article: http://arduino.cc/en/Tutorial/Calibration
+ * Using the anonmeter from http://shop.moderndevice.com/products/wind-sensor
+ *
+ * Note, the calibration is a little finicky and I don't think its working quite right yet
+ *
+*/
+int md0550_data_pin = A0;
+int sensor_value = 0;
+int sensorMin = 1023;        // minimum sensor value
+int sensorMax = 0;           // maximum sensor value
+
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(md0550_data_pin, INPUT);   
+  Serial.begin(115200);
+  Serial.println("Giving sensor a chance to boot up");
+  delay(10000);
+  Serial.println("Calibrating");
+  // calibrate during the first five seconds 
+  while (millis() < 70000) {
+    sensor_value = analogRead(md0550_data_pin);
+
+    // record the maximum sensor value
+    if (sensor_value > sensorMax) {
+      /* Serial.print("Overriding sensorMax ");
+      Serial.println(sensor_value); */
+      sensorMax = sensor_value;
+    }
+
+    // record the minimum sensor value
+    if (sensor_value < sensorMin) {
+      /* Serial.print("Overriding sensorMin ");
+      Serial.println(sensor_value); */
+      sensorMin = sensor_value;
+    }
+  }
+  Serial.print("Calibration settled with sensorMax = ");
+  Serial.print(sensorMax);
+  Serial.print(" and sensorMin = ");
+  Serial.println(sensorMin);
+}
+
+// loop and read the sensor
+void loop() {
+  // read the value from the sensor:
+  sensor_value = analogRead(md0550_data_pin);
+  sensor_value = map(sensor_value, sensorMin, sensorMax, 0, 255);
+  Serial.print("Sensor reading: ");
+  Serial.println(sensor_value);
+  delay(500);
+}
+
