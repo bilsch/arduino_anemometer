84cb4c67550c459f1beedc52811c7f3f1ed135db
Bill Schwanitz <bilsch@bilsch.org>
2013-10-13 09:03:22 -0400

    Updated code from my arduino project
    also commented the pin connections.
    
    So far, this does work though I'm still having issues with calibration.
    I probably need to hook up RV and take this value in to account per
    iteration in loop. The temperature pin is also needed but not currently
    checked.
    
    For now, I have a simple-ish calibration routine. During this
    calibration process, place a cup or something over the sensor as this
    thing ( as the doc warns ;) ) very very sensitive!

--- wind_sensor/wind_sensor.ino
+++ wind_sensor/wind_sensor.ino
@@ -8,21 +8,33 @@
  *
  * Note, the calibration is a little finicky and I don't think its working quite right yet
  *
+ * To allow for no direct serial console the sensor will keep the led solid until booting/calibration is complete
+ * Once the calibration is complete, the led will be pulsed on/off during each poll ( 500 ms, so its pretty fast )
+ *
+ * Pin connections
+ * Out => A0
+ * RV  => not connected, probably should be ;)
+ * TMP => not connected, probably should be ;)
+ *
 */
-int md0550_data_pin = A0;
+String location = "BEDROOM";
+int md0550_data_pin = A0;    // output pin
 int sensor_value = 0;
 int sensorMin = 1023;        // minimum sensor value
 int sensorMax = 0;           // maximum sensor value
 float sensor_value_to_v = 0;
+int led = 13;
+
 
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(md0550_data_pin, INPUT);   
   Serial.begin(115200);
-  Serial.println("Giving sensor a chance to boot up");
+//  Serial.println("Giving sensor a chance to boot up");
   delay(10000);
-  Serial.println("Calibrating");
+//  Serial.println("Calibrating");
   while (millis() < 40000) {
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
     sensor_value = analogRead(md0550_data_pin);
 
     // record the maximum sensor value
@@ -38,21 +50,25 @@ void setup() {
       Serial.println(sensor_value); */
       sensorMin = sensor_value;
     }
+    digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
   }
-  Serial.print("Calibration settled with sensorMax = ");
-  Serial.print(sensorMax);
-  Serial.print(" and sensorMin = ");
-  Serial.println(sensorMin);
+//  Serial.print("Calibration settled with sensorMax = ");
+//  Serial.print(sensorMax);
+//  Serial.print(" and sensorMin = ");
+//  Serial.println(sensorMin);
 }
 
 // loop and read the sensor
 void loop() {
   // read the value from the sensor:
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   sensor_value = analogRead(md0550_data_pin);
   sensor_value = map(sensor_value, sensorMin, sensorMax, 0, 255);
   sensor_value_to_v =  0.0049 * sensor_value;
-  Serial.print("volt: ");
+  Serial.print(location);
+  Serial.print(" volt: ");
   Serial.println(sensor_value_to_v);
-  delay(1000);
+  digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(500);
 }
 
